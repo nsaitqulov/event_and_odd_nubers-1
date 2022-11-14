8b6af0a4490f5849468b571822234dad3009249e
Nurbek <nurbek.saitqulov@gmail.com>
2022-11-14 22:15:16 +0500

    sum_even.py

--- sum_even.py
+++ sum_even.py
@@ -1,15 +1,15 @@
 #A four-digit integer is given. Find the sum of even digits in it.
 def var_int(x):
-    a=(x%10%2+1)%2*x%10
+    a=(x%10%2+1)%2*(x%10)
     x//=10
 
-    b=(x%10%2+1)%2*x%10
+    b=(x%10%2+1)%2*(x%10)
     x//=10
 
-    c=(x%10%2+1)%2*x%10
+    c=(x%10%2+1)%2*(x%10)
     x//=10
 
-    d=(x%10%2+1)%2*x%10
+    d=(x%10%2+1)%2*(x%10)
 
     return a+b+c+d
 #Create a variable "var_int" and assign it a four-digit integer value.
