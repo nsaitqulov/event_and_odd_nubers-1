1d99e3524703ff67925a2c9ff9b875b53c94ce4d
Nurbek <nurbek.saitqulov@gmail.com>
2022-11-14 21:54:24 +0500

    number_odd.py

--- number_odd.py
+++ number_odd.py
@@ -1,5 +1,18 @@
 #A four-digit integer is given. Find the number of odd digits in it.
+def var_int(x):
+    a=x%10%2
+    x//=10
 
+    b=x%10%2
+    x//=10
+
+    c=x%10%2
+    x//=10
+
+    d=x%10%2
+
+    return a+b+c+d
 #Create a variable "var_int" and assign it a four-digit integer value.
 
 #Print the number of odd digits in the variable "var_int".
+print(var_int(4000))
